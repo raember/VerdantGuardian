bcf3ffb0e83fdc089cd166e3a83d45d015cd9954
raember <raember@users.noreply.github.com>
2024-09-16 13:20:49 +0200

    Fixed unittests

--- test/all_tests.py
+++ test/all_tests.py
@@ -1,4 +1,5 @@
 from verdantguardian.utils.logging import *
+from models.solar_test import *
 from services.open_elevation_test import *
 
 if __name__ == '__main__':
--- test/common.py
+++ test/common.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
+from astropy.coordinates import Latitude
 from astropy.time import TimezoneInfo
 from astropy import units as u
 
-ZURICH_COORDS = (47.36616, 8.54150)
+ZURICH_COORDS = (Latitude(47.36616*u.deg), Latitude(8.54150*u.deg))
 ZURICH_ALTITUDE = 404.0
 ZURICH_TZ = TimezoneInfo(utc_offset=2*u.hour)
 ZURICH_TIMESTAMP = datetime(2024, 8, 1, 12, 0, 0, 0, tzinfo=ZURICH_TZ)
